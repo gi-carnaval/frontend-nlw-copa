d1e4d54efd1c64a0c80c3f731001b6e07ad2b5da
Giovani Carnaval <gi.carnaval91@gmail.com>
2022-11-21 20:49:00 -0300

    Atualização para build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document(){
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body className="bg-gray-900 bg-app bg-no-repeat bg-cover">
