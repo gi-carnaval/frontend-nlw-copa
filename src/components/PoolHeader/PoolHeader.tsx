632b04b0d0b92c74b8f8076b206c5adab93665a4
Giovani Carnaval <gi.carnaval91@gmail.com>
2022-11-29 15:02:14 -0300

    Atualizando botão de compartilhar bolão

--- src/components/PoolHeader/PoolHeader.tsx
+++ src/components/PoolHeader/PoolHeader.tsx
@@ -1,15 +1,48 @@
 import { PoolCardProps } from '../PoolCard/PoolCard';
 import { Participants } from '../Participants/Participants';
-import { FaWhatsapp } from 'react-icons/fa'
+import { FaWhatsapp, FaRegCopy } from 'react-icons/fa'
+import { toast } from 'react-toastify';
 
 import styles from './styles.module.scss'
+import 'react-toastify/dist/ReactToastify.css';
 
 interface Props {
   data: PoolCardProps;
-  share: () => void;
+  // share: () => void;
 }
 
-export function PoolHeader({ data, share }: Props) {
+export function PoolHeader({ data }: Props) {
+
+  function checkDevice() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       return true;
+     }
+    else {
+       return false;
+     }
+   }
+
+  async function share() {
+    if(checkDevice()){
+      
+      navigator.share({
+        text: `Entre no meu bolão e se divirta com os palpites dos jogos!! ${'\n'}.${'\n'}.${'\n'}.${'\n'}Código do Bolão: *${data.code}* ${'\n'}.${'\n'}.${'\n'} https://frontend-nlw-copa.vercel.app/find/${data.code}`
+      })
+    } else {
+      await navigator.clipboard.writeText(`Entre no meu bolão e se divirta com os palpites dos jogos!! ${'\n'}.${'\n'}.${'\n'}.${'\n'}Código do Bolão: *${data.code}* ${'\n'}.${'\n'}.${'\n'} https://frontend-nlw-copa.vercel.app/find/${data.code}`)
+      return toast.success('Código do bolão copiado com sucesso. Cole em seu Whatsapp e manda pra galera', {
+        theme: "colored",
+      });  
+    }
+   }
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -17,13 +50,14 @@ export function PoolHeader({ data, share }: Props) {
           {data.title}
         </h1>
 
-        <div className={styles.codeContent}>
+        <div className={styles.codeContent} onClick={share}>
           <span className={styles.subtitle}>
             Código: &nbsp;
           </span>
 
           <span className={styles.code}>
-            {data.code}
+            {data.code} &nbsp;&nbsp;
+            <FaRegCopy/>
           </span>
         </div>
       </div>
--- src/pages/details/[code].tsx
+++ src/pages/details/[code].tsx
@@ -66,35 +66,6 @@ export default function Details({ id, bearer }: DetailsProps){
       setIsLoading(false);
     }
   }
-  function checkDevice() { 
-    if( navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-    ){
-       return true;
-     }
-    else {
-       return false;
-     }
-   }
-
- async function share() {
-  if(checkDevice()){
-    
-    navigator.share({
-      text: `Entre no meu bolão e se divirta com os palpites dos jogos!! ${'\n'}.${'\n'}.${'\n'}.${'\n'}Código do Bolão: *${poolDetails.code}* ${'\n'}.${'\n'}.${'\n'} https://frontend-nlw-copa.vercel.app/find/${poolDetails.code}`
-    })
-  } else {
-    await navigator.clipboard.writeText(`Entre no meu bolão e se divirta com os palpites dos jogos!! ${'\n'}.${'\n'}.${'\n'}.${'\n'}Código do Bolão: *${poolDetails.code}* ${'\n'}.${'\n'}.${'\n'} https://frontend-nlw-copa.vercel.app/find/${poolDetails.code}`)
-    return toast.success('Código do bolão copiado com sucesso. Cole em seu Whatsapp e manda pra galera', {
-      theme: "colored",
-    });  
-  }
- }
 
  function handleHiddeGame(){
   setHiddeGames(!hiddeGames)
@@ -111,7 +82,7 @@ export default function Details({ id, bearer }: DetailsProps){
         poolDetails._count?.participants > 0 ? 
         <div className={styles.detailsContent}>
           <div className={styles.headerDetails}>
-            <PoolHeader data={poolDetails} share={share}/>
+            <PoolHeader data={poolDetails}/>
 
             <div className={styles.sectionOptions}>
               <Option 
